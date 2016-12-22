3c4403dd5051a430c5a08685641c13e36fb4d835
sinlov <sinlovgmppt@gmail.com>
2016-12-23 00:32:38 +0800

    Initial commit

---
+++ Vue-Router-Demo/src/components/other.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header-component/>
+        <div>this is other</div>
+        <other-component/>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#000000;
+    }
+</style>
+<script>
+export default {
+  name: 'head',
+  data () {
+    return {
+      msg: 'this is head'
+    }
+  }
+}
+</script>
